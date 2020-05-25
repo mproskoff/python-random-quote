e32aa8847418465e3cfa63c61f34f60d1c8a6534
Max Proskoff <max@MAXP.pmc.local>
2020-05-25 17:25:55 -0500

    Added enum List Cycle

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,21 @@
 def main():
-  print("Keep it logically awesome.")
+  #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  #print(len(quotes))
+
+#  for z in range(len(quotes)):
+#    print(str(z+1)+": " + quotes[z])
+
+  for num, qt in enumerate(quotes, start=1):
+    print("Quote# {}: {}".format(num, qt))
+
+
+  #print(quotes[len(quotes)-1])
 
 if __name__== "__main__":
-  charlie()
+  main()
 
